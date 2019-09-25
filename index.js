05402b35b7f0b92f01e7f36b5ad84a1487e44d65
Sani Ahamed <thesua711@gmail.com>
2019-09-26 00:59:09 +0600

    Adding edge template

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ const express = require('express');
 const path = require('path')
 const app = new express();
 
+//{
+// const expressEdge = require('express-edge'); 
+// //npm install express-edge --save {Edge templating}
+
+// app.use(expressEdge);
+// app.set('views',`${__dirname}/views`);
+
+// //}
+
+const { config, engine } = require('express-edge');
+
+// Configure Edge if need to
+config({ cache: process.env.NODE_ENV === 'production' });
+
+// Automatically sets view engine and adds dot notation to app.render
+app.use(engine);
+app.set('views', `${__dirname}/views`);
+
 
 app.use(express.static('public'));
 
 
 app.get('/',(req,res) =>{
-    res.sendFile(path.resolve(__dirname,'pages/index.html'))
+    res.render('index')
 })
 
 
