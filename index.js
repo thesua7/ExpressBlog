f4447f30257783715433676cb68c9656c98b4946
Sani Ahamed <thesua711@gmail.com>
2019-09-25 23:55:19 +0600

    Creating a home page

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 
+const path = require('path')
 const app = new express();
+
+
+app.use(express.static('public'));
+
+
+app.get('/',(req,res) =>{
+    res.sendFile(path.resolve(__dirname,'pages/index.html'))
+})
 
 
 app.listen(4000,()=>{
